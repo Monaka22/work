0e3bd0dfb3ef937c9d8bf87983e318266c525aa0
Monaka22 <MrTawatchai22@outlook.com>
2019-01-14 15:09:50 +0700

    Session but Why i can't logout!!!

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const express = require('express'),
   myConnection = require('express-myconnection');
 
 const app = express();
-const session  = require('express-session');
+const session = require('express-session');
 
 const Routers = require('./routers/routers');
 
 app.use(session({
-  secret: 'codemobiles', cookie: { maxAge: 3600000 },
-  resave: true, saveUninitialized: false
+  secret: 'codemobiles',
+  cookie: {
+    maxAge: 3600000
+  },
+  resave: true,
+  saveUninitialized: false
 }));
 
 app.use(morgan('dev'));
@@ -23,27 +27,25 @@ app.use(myConnection(mysql, {
 }, 'single'));
 //app.use(express.urlencoded({ extended: true }));
 
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
 
   const _username = req.query.username;
   const _password = req.query.password;
 
-  if (req.query.username == "admin" && req.query.password == "qwerty"){
+  if (req.query.username == "admin" && req.query.password == "qwerty") {
     req.session.username = req.query.username;
     req.session.isLoggedIn = true;
+    app.use('/', Routers);
     res.end("Can use API.");
-  }else{
+  } else {
     res.send("Invalid username and password");
   }
 });
-app.get('/logout', function(req, res){
+app.get('/logout', function (req, res) {
   req.session.destroy();
-  app.use(errorHandle());
-  res.end("Logout complete")});
-app.get('/addemp',function (req,res) {
-  res.render('emp_add.ejs');
+  res.end("Logout complete")
 });
-app.use('/',Routers);
+
 
 var server = app.listen(9000, function () {
   var host = server.address().address;
