85115c8bca5e9488caa82a8f6e29e95b8a6957ab
Monaka22 <MrTawatchai22@outlook.com>
2019-01-14 14:13:17 +0700

    Session test

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const express = require('express'),
   myConnection = require('express-myconnection');
 
 const app = express();
-//const session  = require('express-session');
+const session  = require('express-session');
 
 const Routers = require('./routers/routers');
 
-// app.use(session({
-//   secret: 'codemobiles', cookie: { maxAge: 3600000 },
-//   resave: true, saveUninitialized: false
-// }));
+app.use(session({
+  secret: 'codemobiles', cookie: { maxAge: 3600000 },
+  resave: true, saveUninitialized: false
+}));
 
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
@@ -23,27 +23,26 @@ app.use(myConnection(mysql, {
 }, 'single'));
 //app.use(express.urlencoded({ extended: true }));
 
-// app.get('/login', function(req, res){
+app.get('/login', function(req, res){
 
-//   const _username = req.query.username;
-//   const _password = req.query.password;
+  const _username = req.query.username;
+  const _password = req.query.password;
 
-//   if (req.query.username == "admin" && req.query.password == "qwerty"){
-//     req.session.username = req.query.username;
-//     req.session.isLoggedIn = true;
-//     app.use('/', Routers);
-//     res.end("Can use API.");
-//   }else{
-//     res.send("Invalid username and password");
-//   }
-// });
-// app.get('/logout', function(req, res){
-//   req.session.destroy();
-//   app.use(errorHandle());
-//   res.end("Logout complete")});
-// app.get('/addemp',function (req,res) {
-//   res.render('emp_add.ejs');
-// });
+  if (req.query.username == "admin" && req.query.password == "qwerty"){
+    req.session.username = req.query.username;
+    req.session.isLoggedIn = true;
+    res.end("Can use API.");
+  }else{
+    res.send("Invalid username and password");
+  }
+});
+app.get('/logout', function(req, res){
+  req.session.destroy();
+  app.use(errorHandle());
+  res.end("Logout complete")});
+app.get('/addemp',function (req,res) {
+  res.render('emp_add.ejs');
+});
 app.use('/',Routers);
 
 var server = app.listen(9000, function () {
